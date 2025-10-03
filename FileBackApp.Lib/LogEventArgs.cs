e40576030104141b70b8fb8f25d3a776bf302eee
CatNuton <nytshow612@gmail.com>
2025-10-03 23:30:23 +0300

    Added library

---
+++ FileBackApp.Lib/LogEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileBackApp.Lib
+{
+    public class LogEventArgs : EventArgs
+    {
+        public string Message { get; }
+        public ConsoleColor Color { get; }
+        public LogEventArgs(string message, ConsoleColor color)
+        {
+            Message = message;
+            Color = color;
+        }
+    }
+}
